566f451b2690b595e8f27534353ab2fd2998da8e
programmercube <roshan@programmercube.com>
2025-05-12 12:08:21 +0530

    Vim installation

--- Test.php
+++ Test.php
@@ -1,3 +1,10 @@
-<?php
-    echo "Roshan here";
-?>
+1. How to install vim 
+
+>> Command [ sudo yum install vim ]
+    Note : To check vim intalled and which Location use >> which vim   
+    output :  /usr/bin/vim
+
+
+2.     
+
+
